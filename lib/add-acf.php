9d75aef81bb16d8f21b360ad13384afc4f6db36e
Devin Cremins <drcremin@ncsu.edu>
2017-04-24 14:12:19 -0400

    act pro and namespaces

---
+++ lib/add-acf.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Research\ACF;
+
+// Check if ACF is used with another plugin, if not already called, use this one
+if (!class_exists('acf')) {
+    add_filter('acf/settings/path', __NAMESPACE__ . '\\my_acf_settings_path');
+
+    function my_acf_settings_path($path)
+    {
+        // update path
+        $path = plugin_dir_path(dirname(__FILE__)) . '/acf/';
+
+        // return
+        return $path;
+    }
+
+    add_filter('acf/settings/dir', __NAMESPACE__ . '\\my_acf_settings_dir');
+
+    function my_acf_settings_dir($dir)
+    {
+        // update path
+        $dir = plugin_dir_url(dirname(__FILE__)) . '/acf/';
+
+        // return
+        return $dir;
+    }
+
+    include_once(plugin_dir_path(dirname(__FILE__)) . '/acf/acf.php');
+}
+
+//Include the /acf folder in the places to look for ACF Local JSON files
+add_filter('acf/settings/load_json', function ($paths) {
+    $paths[] = plugin_dir_path(dirname(__FILE__)) . '/acf/acf-json';
+    return $paths;
+});
+
+add_filter('acf/settings/save_json', function ($path) {
+    $path = plugin_dir_path(dirname(__FILE__)) . '/acf/acf-json';
+    return $path;
+});
--- research.php
+++ research.php
@@ -4,21 +4,33 @@ Plugin Name: ITRE Research Documents
 GitHub Plugin URI: https://github.com/dcremins/research
 GitHub Branch:      master
 Description: Custom Post Type and Views for ITRE website use
-Version: 0.1
+Version: 0.1.1
 Author: Devin Cremins
 Author URI: http://octopusoddments.com
 */
 
-require_once( dirname( __FILE__ ) . '/lib/functions.php' );
+// Add all files in lib folder into array
+$include = [
+  '/lib/cpt.php',       // Register Post Type
+  '/lib/add-acf.php',       // Register Advanced Custom Fields
+];
+
+// Require Once each file in the array
+foreach ($include as $file) {
+    if (!$filepath = (dirname(__FILE__) .$file)) {
+        trigger_error(sprintf('Error locating %s for inclusion', $file), E_USER_ERROR);
+    }
+
+    require_once $filepath;
+}
+unset($file, $filepath);
 
 /* Add Style.css */
-add_action('wp_enqueue_scripts', function(){
-	wp_enqueue_style( 'myCSS', plugins_url( '/styles/main.css', __FILE__ ) );
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('myCSS', plugins_url('/styles/main.css', __FILE__));
 });
 
 /* Add Admin.css */
 add_action('admin_enqueue_scripts', function () {
-	wp_enqueue_style( 'adminCSS', plugins_url( '/styles/admin.css', __FILE__ ) );
+    wp_enqueue_style('adminCSS', plugins_url('/styles/admin.css', __FILE__));
 });
-
-?>
