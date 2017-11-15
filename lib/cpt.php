4379217249ec9b548d50d39995894d443f5a91c3
Devin Cremins <drcremin@ncsu.edu>
2017-11-15 11:07:58 -0500

    filter to add cpt to query

--- lib/cpt.php
+++ lib/cpt.php
@@ -24,8 +24,32 @@ function research_post_type()
       'has_archive'   => true,
       'menu_icon'     => 'dashicons-book-alt',
       'supports'      => array('title', 'editor', 'author'),
-      'taxonomies'          => array('category'),
+      'taxonomies'    => array('category'),
     ]);
 }
 
 add_action('init', __NAMESPACE__ . '\\research_post_type');
+
+function loop_filter($query) {
+  if ( !is_admin() && $query->is_main_query() ) {
+		// Fetch current query post types
+		$post_types = $query->get('post_type');
+			// If there are already post types set and they aren't in an array
+			// Split at the comma and put them into an array
+      if (!is_array($post_types) && !empty($post_types)) {
+				$post_types = explode(',', $post_types);
+			}
+			// If there are no post types set, create an array containing 'Post' post type
+      if (empty($post_types))
+          $post_types[] = 'post';
+			// Add our custom post type
+      $post_types[] = 'research';
+			// Clean up the array, just in case
+      $post_types = array_map('trim', $post_types);
+      $post_types = array_filter($post_types);
+			// Set query to our new post type array
+      $query->set('post_type', $post_types);
+  }
+}
+
+add_action('pre_get_posts', __NAMESPACE__ . '\\loop_filter');
