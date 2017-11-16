f67bbc10b49dfda8f1bf31c4d6ccccc32a79ea7c
Devin Cremins <drcremin@ncsu.edu>
2017-11-16 11:15:11 -0500

    pagination

--- lib/archive-research.php
+++ lib/archive-research.php
@@ -4,6 +4,29 @@
  Template Post Type: research
  */
 
+ function page_navi() {
+	global $wp_query;
+	$bignum = 999999999;
+	if ( $wp_query->max_num_pages <= 1 )
+		return;
+
+	echo '<nav class="pagination">';
+
+		echo paginate_links( array(
+			'base' 			=> str_replace( $bignum, '%#%', esc_url( get_pagenum_link($bignum) ) ),
+			'format' 		=> '',
+			'current' 		=> max( 1, get_query_var('paged') ),
+			'total' 		=> $wp_query->max_num_pages,
+			'prev_text' 	=> '&larr;',
+			'next_text' 	=> '&rarr;',
+			'type'			=> 'list',
+			'end_size'		=> 3,
+			'mid_size'		=> 3
+		) );
+
+	echo '</nav>';
+}
+
 	if (!have_posts()) {
 		echo '<div class="alert alert-warning">';
 		_e('Sorry, no results were found.', 'sage');
@@ -35,5 +58,5 @@
 					echo '</div>';
 			echo '</article>';
 		}
-		the_posts_navigation();
+		page_navi();
 	}
--- research.php
+++ research.php
@@ -4,7 +4,7 @@ Plugin Name: ITRE Research Documents
 GitHub Plugin URI: https://github.com/dcremins/research
 GitHub Branch: master
 Description: Custom Post Type and Views for ITRE website use
-Version: 1.0.5
+Version: 1.0.6
 Author: Devin Cremins
 Author URI: http://octopusoddments.com
 */
