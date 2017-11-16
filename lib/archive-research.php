f7d607c340c5d15f52705cb7449be2ca8e70b6e7
Devin Cremins <drcremin@ncsu.edu>
2017-11-16 10:51:15 -0500

    fix cat desc box

--- lib/archive-research.php
+++ lib/archive-research.php
@@ -9,30 +9,31 @@
 		_e('Sorry, no results were found.', 'sage');
 		echo '</div>';
 		get_search_form();
-	}
-	while (have_posts()) {
+	} else {
 		if ( category_description() ) {
 			// If the category has a description, show it above the posts
 			echo '<div class="category-description">';
 			echo '<p>' . category_description() . '</p>';
 			echo '</div>';
 		}
-		the_post();
-		echo '<article ';
-		post_class();
-		echo ' >';
-		  echo ' <header class="post_head">'
-		    . '<h3 class="entry-title">'
-				. '<a href="';
-				the_permalink();
-				echo '">';
-				the_title();
-				echo '</a>'
-				. '</h3>'
-		  	. '</header>';
-		  echo '<div class="entry-summary">';
-		  	the_excerpt();
-				echo '</div>';
-		echo '</article>';
+		while (have_posts()) {
+			the_post();
+			echo '<article ';
+			post_class();
+			echo ' >';
+			  echo ' <header class="post_head">'
+			    . '<h3 class="entry-title">'
+					. '<a href="';
+					the_permalink();
+					echo '">';
+					the_title();
+					echo '</a>'
+					. '</h3>'
+			  	. '</header>';
+			  echo '<div class="entry-summary">';
+			  	the_excerpt();
+					echo '</div>';
+			echo '</article>';
+		}
+		the_posts_navigation();
 	}
-	the_posts_navigation();
