3bd363d4dc8474a6cd41c221a21b3f04fddf05d3
Devin Cremins <drcremin@ncsu.edu>
2018-01-30 11:12:23 -0500

    no search in theme

--- lib/templates.php
+++ lib/templates.php
@@ -11,16 +11,16 @@ function include_template_function($template_path) {
 		if (is_single()) {
 			// checks if the file exists in the theme first,
 			// otherwise serve the file from the plugin
-			if ($theme_file = locate_template(array ('single-research.php'))) {
-				return $theme_file;
-			}
+			//if ($theme_file = locate_template(array ('single-research.php'))) {
+			//	return $theme_file;
+			//}
 			return plugin_dir_path(dirname(__FILE__)) . 'lib/single-research.php';
 		} elseif (is_archive() && !is_search() && !is_author()) {
 			// checks if the file exists in the theme first,
 			// otherwise serve the file from the plugin
-			if ($theme_file = locate_template(array ('archive-research.php'))) {
-				return $theme_file;
-			}
+			//if ($theme_file = locate_template(array ('archive-research.php'))) {
+			//	return $theme_file;
+			//}
 			return plugin_dir_path(dirname(__FILE__)) . 'lib/archive-research.php';
 		}
 	}
